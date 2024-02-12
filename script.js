31a89fe2b97e603878140585e250b2b72bc23d3a
Moshe Rosenberg <mosherosenberg14@gmail.com>
2024-02-11 19:25:38 -0500

    Added files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const questionTitle = document.querySelector('.question')
+const optionsElement = document.querySelector('.options')
+const submitBtn = document.querySelector('.submit')
+
+let selectedBtn = [null]
+
+const data = {
+    question: 'Select a mapach',
+    options: ['munach', 'pashta', 'mapach', 'karnei para'],
+    correct: 2
+}
+
+function loadButtons() {
+    questionTitle.innerHTML = data.question
+    data.options.forEach(option => {
+        const btn = document.createElement('button')
+        btn.innerHTML = option
+        btn.onclick = () => { selected(btn) }
+        optionsElement.appendChild(btn)
+
+    })
+    // randomize order of elements
+}
+
+function selected(target) {
+    if (submitBtn.disabled) { submitBtn.disabled = false }
+
+    if (selectedBtn[0] != target) {
+        if (selectedBtn[0] == null) {
+            selectedBtn = [target]
+            selectedBtn[0].classList.add('selected')
+            return
+        }
+        selectedBtn[0].classList.remove('selected')
+        selectedBtn = [target]
+        selectedBtn[0].classList.add('selected')
+    } else {
+        selectedBtn[0].classList.remove('selected')
+        selectedBtn = [null]
+        submitBtn.disabled = true
+    }
+}
+
+submitBtn.onclick = () => {
+    if (selectedBtn[0].innerHTML == data.options[data.correct]) {
+        alert('correct')
+    } else {
+        alert('incorrect')
+    }
+    location.reload()
+}
+
+loadButtons()
